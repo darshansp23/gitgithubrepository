61e57c66d28151aa5297a1a4307034ea5ce80092
Darshan Pokharkar <121633659+darshansp23@users.noreply.github.com>
2023-04-24 17:58:17 +0530

    Update secondprogram.java

--- secondprogram.java
+++ secondprogram.java
@@ -1,24 +1,21 @@
-public class SwapNumbers {
+// Java program to swap two variables
+public class GfG {
 
-    public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 
-        float first = 1.20f, second = 2.45f;
+		int x = 100, y = 200;
 
-        System.out.println("--Before swap--");
-        System.out.println("First number = " + first);
-        System.out.println("Second number = " + second);
+		System.out.println("Before Swap");
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
 
-        // Value of first is assigned to temporary
-        float temporary = first;
+		int temp = x;
+		x = y;
+		y = temp;
 
-        // Value of second is assigned to first
-        first = second;
-
-        // Value of temporary (which contains the initial value of first) is assigned to second
-        second = temporary;
-
-        System.out.println("--After swap--");
-        System.out.println("First number = " + first);
-        System.out.println("Second number = " + second);
-    }
+		System.out.println("After swap");
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+	}
 }
